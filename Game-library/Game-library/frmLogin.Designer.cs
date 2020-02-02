dbcb1565a547c860c94eef66aaa52e32fe2a3e1a
Eryk-luiz <erykluizvieira@gmail.com>
2020-02-02 15:59:13 -0200

    Mudança do Design do frmLogin

--- Game-library/Game-library/frmLogin.Designer.cs
+++ Game-library/Game-library/frmLogin.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.text_login = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label_login = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(109, 308);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(122, 44);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // text_login
+            // 
+            this.text_login.Location = new System.Drawing.Point(72, 203);
+            this.text_login.Name = "text_login";
+            this.text_login.PasswordChar = '0';
+            this.text_login.Size = new System.Drawing.Size(197, 20);
+            this.text_login.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '1';
+            this.textBox2.Size = new System.Drawing.Size(197, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label_login
+            // 
+            this.label_login.AutoSize = true;
+            this.label_login.Location = new System.Drawing.Point(72, 184);
+            this.label_login.Name = "label_login";
+            this.label_login.Size = new System.Drawing.Size(40, 13);
+            this.label_login.TabIndex = 3;
+            this.label_login.Text = "LOGIN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SENHA";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 102);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LOGO";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(810, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(341, 402);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.text_login);
+            this.Controls.Add(this.btn_login);
+            this.Name = "frmLogin";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox text_login;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
