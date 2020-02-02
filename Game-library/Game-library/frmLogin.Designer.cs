4bbe9ffb06b65e7bf797db96ab24d190e111cff2
Eryk-luiz <erykluizvieira@gmail.com>
2020-02-02 16:02:26 -0200

    ...

--- Game-library/Game-library/frmLogin.Designer.cs
+++ Game-library/Game-library/frmLogin.Designer.cs
@@ -82,9 +82,9 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(59, 33);
+            this.label2.Location = new System.Drawing.Point(12, 33);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(223, 102);
+            this.label2.Size = new System.Drawing.Size(317, 136);
             this.label2.TabIndex = 5;
             this.label2.Text = "LOGO";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
