c1c4334e2d2638142682a749fd54d8f8e3da98b2
Eryk Luiz <erykluizvieira@gmail.com>
2020-02-06 18:57:09 -0200

    Teste 123

--- Game-library/Game-library/library.Designer.cs
+++ Game-library/Game-library/library.Designer.cs
@@ -33,51 +33,68 @@
             this.libraryPanel = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.libraryPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(77, 60);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(208, 74);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1057, 613);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1304, 754);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // libraryPanel
             // 
+            this.libraryPanel.Controls.Add(this.textBox2);
+            this.libraryPanel.Controls.Add(this.textBox1);
             this.libraryPanel.Controls.Add(this.flowLayoutPanel1);
             this.libraryPanel.Controls.Add(this.button1);
             this.libraryPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.libraryPanel.Location = new System.Drawing.Point(0, 0);
-            this.libraryPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.libraryPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.libraryPanel.Name = "libraryPanel";
-            this.libraryPanel.Size = new System.Drawing.Size(1134, 673);
+            this.libraryPanel.Size = new System.Drawing.Size(1512, 828);
             this.libraryPanel.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(14, 9);
+            this.button1.Location = new System.Drawing.Point(19, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 45);
+            this.button1.Size = new System.Drawing.Size(157, 55);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // timer1
+            // textBox1
             // 
-            
-            
+            this.textBox1.Location = new System.Drawing.Point(19, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 199);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 3;
             // 
             // library
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.libraryPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "library";
-            this.Size = new System.Drawing.Size(1134, 673);
+            this.Size = new System.Drawing.Size(1512, 828);
             this.libraryPanel.ResumeLayout(false);
+            this.libraryPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -88,5 +105,7 @@
         private System.Windows.Forms.Panel libraryPanel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Game-library/Game-library/library.cs
+++ Game-library/Game-library/library.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlServerCe;
 
 namespace Game_library
 {
@@ -44,22 +45,32 @@ namespace Game_library
             List<GameBanner> list = new List<GameBanner>();
 
 
-            
-            
-                for (int i = 0; i < 1; i++)
+
+            SqlCeConnection connection = new SqlCeConnection("Data Source = " + CreateDataBase.conString);
+            connection.Open();
+
+
+            DataTable table = new DataTable();
+            string query = "SELECT USER_NAME FROM Users";
+            SqlCeDataAdapter command = new SqlCeDataAdapter(query, connection);
+            command.Fill(table);
+
+            connection.Close();
+           
+
+                for (int i = 0; i < table.Rows.Count; i++)
                 {
-                    game.genre = "Action";
-                    game.title = "Resident Evil";
+                    game.genre = table.Rows[0]["USER_NAME"].ToString();
+                    game.title = textBox1.Text;
                     game.description = "Horror Game";
                     list.Add(game);
 
                     flowLayoutPanel1.Controls.Add(list[i]);
-
                 }
             
             
 
-                
+        
         }
 
        
