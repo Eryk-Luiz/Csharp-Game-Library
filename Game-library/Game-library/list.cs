7aa183d4ad3fb848eca89f918d083de51884b8d2
Eryk-luiz <erykluizvieira@gmail.com>
2020-02-06 22:28:46 -0200

    teste

---
+++ Game-library/Game-library/list.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_library
+{
+    public static  class list
+    {
+        
+        
+        public static void lista(int count)
+        {
+            GameBanner game = new GameBanner();
+            List<GameBanner> list = new List<GameBanner>();
+
+
+
+
+            for (int i = 0; i < count; i++)
+            {
+                game.genre = "Action";
+                game.title = "Resident Evil";
+                game.description = "Horror Game";
+                list.Add(game);
+            }
+        }
+
+    }
+
+
+
+    }
+
